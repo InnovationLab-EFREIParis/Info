b82170dcb221558acb320727d53891949b735d9c
RemiEfrei <remi.griot@efrei.fr>
2023-01-16 18:18:47 +0100

    update

--- publish.el
+++ publish.el
@@ -1,4 +1,27 @@
-; load the publishing system
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+
+;; Code block darker 80%
+(require 'color)
+(set-face-attribute 'org-block nil :background
+                    (color-darken-name
+                     (face-attribute 'default :background) 80))
+
+
+;load the publishing system
 (require 'ox-publish)
 
 ; Define the publishing project
@@ -30,25 +53,19 @@
 
 	))
 
+;; Customize the HTML output
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style nil ;; Use our own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+
+
+
+
 ;; Generate the site output
 (org-publish-all t)
 
 (message "Build complete!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
